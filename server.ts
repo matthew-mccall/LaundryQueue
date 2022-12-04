91ecfdcdf5aff0299900b135c2ee24609b5954da
Matthew McCall <matthew+github@mmccall.dev>
2022-12-04 16:28:36 -0500

    Switched http server for polka

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
-import { createServer } from 'http'
-import { parse } from 'url'
 import next from 'next'
+import polka from 'polka'
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
@@ -8,14 +7,9 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
-
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+  polka()
+      .get('*', handle)
+      .listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`);
+      });
+});
