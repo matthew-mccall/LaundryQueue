a34be8a363943bd1abbbf34ad525bfc55f2ef7ec
Matthew McCall <matthew+github@mmccall.dev>
2022-12-05 10:57:42 -0500

    Added bot boilerplate

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,1 +1,33 @@
-export {}
+import Eris from 'eris'
+import * as dotenv from 'dotenv'
+
+export default function bot()
+{
+    dotenv.config()
+
+    const bot = Eris(`Bot ${process.env.BOT_TOKEN}`, {
+        intents: [
+            "guildMessages"
+        ]
+    });
+
+    bot.on("ready", () => { // When the bot is ready
+        console.log("Ready!"); // Log "Ready!"
+    });
+
+    bot.on("error", (err) => {
+        console.error(err); // or your preferred logger
+    });
+
+    bot.on("messageCreate", (msg) => { // When a message is created
+        if(msg.content === "!ping") { // If the message content is "!ping"
+            bot.createMessage(msg.channel.id, "Pong!");
+            // Send a message in the same channel with "Pong!"
+        } else if(msg.content === "!pong") { // Otherwise, if the message is "!pong"
+            bot.createMessage(msg.channel.id, "Ping!");
+            // Respond with "Ping!"
+        }
+    });
+
+    bot.connect(); // Get the bot to connect to Discord
+}
