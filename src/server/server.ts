6708797da24175a015d0c4ffe7c0a62c9dd8a21d
Matthew McCall <matthew+github@mmccall.dev>
2022-12-05 10:44:41 -0500

    Separated server and bot. Moved source into src

---
+++ src/server/server.ts
@@ -0,0 +1,20 @@
+import next from 'next'
+import polka from 'polka'
+import sirv from 'sirv'
+
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+export default function server()
+{
+    app.prepare().then(() => {
+        polka()
+            .use(sirv('public'))
+            .get('*', handle)
+            .listen(port, () => {
+                console.log(`> Ready on http://localhost:${port}`);
+            });
+    });
+}
