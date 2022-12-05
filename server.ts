036f955400e6b07b17f255cdcd1e3bf77710c683
Matthew McCall <matthew+github@mmccall.dev>
2022-12-04 22:58:55 -0500

    Added sirv

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import next from 'next'
 import polka from 'polka'
+import sirv from 'sirv'
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
@@ -8,6 +9,7 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   polka()
+      .use(sirv('public'))
       .get('*', handle)
       .listen(port, () => {
         console.log(`> Ready on http://localhost:${port}`);
